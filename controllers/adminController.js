984632939aabe70a6769af5db8ad1bb8630cf235
TL Chang <coooo77@hotmail.com>
2020-05-27 01:40:58 +0800

    Add admin tweets page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,19 @@
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
+
 const adminController = {
-  getTweets: (req, res) => {
-    return res.render('admin/tweets')
+  getTweets: async (req, res) => {
+    const tweets = await Tweet.findAll({
+      raw: true,
+      nest: true,
+      include: [User]
+    })
+    const data = tweets.map(tweet => ({
+      ...tweet,
+      description: tweet.description.substring(0, 50)
+    }))
+    return res.render('admin/tweets', { tweet: data })
   }
 }
 
